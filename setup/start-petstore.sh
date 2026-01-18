3cdd0557bfb78ffeee2e638f397f82da7511fa83
Evans Balcazar Veizaga <evansbv@gmail.com>
2026-01-18 11:22:17 -0400

    start-petstore.sh
    start-petstore.sh

---
+++ setup/start-petstore.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+CONTAINER_NAME="petstore3"
+IMAGE_NAME="swaggerapi/petstore3:unstable"
+
+if docker ps --filter "name=${CONTAINER_NAME}" --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    echo "Ya está corriendo."
+    exit 0
+fi
+
+docker run -d --name "${CONTAINER_NAME}" -p 8080:8080 "${IMAGE_NAME}"
+echo "Iniciado → http://localhost:8080"
